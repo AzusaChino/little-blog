ea5cf5c025332bf961b14ffcbfb5d22cd759960b
AzusaChino <azusa146@gmail.com>
2021-01-27 22:14:53 +0800

    添加redis配置, 尚无法启动

--- blog-web/web/src/App.tsx
+++ blog-web/web/src/App.tsx
@@ -1,28 +1,50 @@
 import React from "react";
 import "./App.css";
-import { Layout, Menu } from "antd";
-import {Router} from 'react-router'
+import {Layout, Menu} from "antd";
+import {Router, Link, Switch, Route} from 'react-router'
 import Article from "./components/article";
+import {About} from './views/about'
+import {Comment} from './views/comment'
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
+
 function App() {
-  return (
-    <div className="App">
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal">
-            <Menu.Item key="1">Nav 1</Menu.Item>
-            <Menu.Item key="2">Nav 2</Menu.Item>
-          </Menu>
-        </Header>
-        <Content>
-          <Article />
-        </Content>
-        <Footer>built by az</Footer>
-      </Layout>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout className="layout">
+                <Header>
+                    <div className="logo"/>
+                    <Router history="">
+                        <Menu theme="dark" mode="horizontal">
+                            <Menu.Item key="1">
+                                <Link to="/">Home</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Link to="/about">Home</Link>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Link to="/comment">Home</Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Router>
+                </Header>
+                <Content>
+                    <Switch>
+                        <Route path="/">
+                            <Article/>
+                        </Route>
+                        <Route path="/about">
+                            <About/>
+                        </Route>
+                        <Route path="/comment">
+                            <Comment/>
+                        </Route>
+                    </Switch>
+                </Content>
+                <Footer>built by az</Footer>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
--- blog-web/web/src/index.tsx
+++ blog-web/web/src/index.tsx
@@ -4,32 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const routeConfig = [
-  { path: '/',
-    component: App,
-    indexRoute: { component: Dashboard },
-    childRoutes: [
-      { path: 'about', component: About },
-      { path: 'article',
-        component: Inbox,
-        childRoutes: [
-          { path: '/messages/:id', component: Message },
-          { path: 'messages/:id',
-            onEnter: function (nextState, replaceState) {
-              replaceState(null, '/messages/' + nextState.params.id)
-            }
-          }
-        ]
-      }
-    ]
-  }
-]
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
