4cbc93c8db149e79aa6b6610f5aebca99ba33682
ycpang <ycpang@iflytek.com>
2021-01-28 15:33:10 +0800

    feat. 调整项目路径(可以正常启动)

---
+++ blog-admin/admin/src/test/java/cn/az/blog/admin/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.az.blog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ blog-web/web/src/model/article.ts
@@ -0,0 +1,16 @@
+export interface ArticleList {
+  Id: string;
+  Topic: string;
+  Thumbnail: string;
+  PublishState: number;
+  PublishTime: Date;
+  CreateUser: string;
+  CreateTime: Date;
+  UpdateUser: string;
+  UpdateTime: Date;
+  IsDelete: number;
+}
+
+export interface ArticleDetail extends ArticleList {
+    Content: string
+}
